b278edb849d1608abc3d7b738f9f8889814e8702
jameson-m-jolley <jameson.m.jolley@gmail.com>
2026-02-10 20:56:27 -0700

    init of the repo

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import numpy as np
+import pandas as pd
+
+
+
+class preceptron(object):
+    def __init__(self, lerning_rate = 0.1, n_itterations = 50, seed =time.time_ns()):
+        self.lerning_rate = lerning_rate
+        self.n_itterations = n_itterations
+        self.seed = seed
+        self.error = 0
+        
+
+
+def main():
+    print("demo of the perceptron")
+
+
+if __name__ == "__main__":
+    main()
